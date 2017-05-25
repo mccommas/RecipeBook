f3d6f632079bd54c24f3e323e3bd231a21156d81
mccommas <jeffmccommas@gmail.com>
2017-05-25 10:15:00 -0400

    added self-closing bootstrap alerts on sign in form

--- src/app/auth/sc-alert/sc-alert.component.ts
+++ src/app/auth/sc-alert/sc-alert.component.ts
@@ -1,36 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import {Subject} from 'rxjs/Subject';
 import 'rxjs/add/operator/debounceTime';
-import { NgForm } from '@angular/forms';
-import { AuthService } from '../auth.service';
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  selector: 'app-sc-alert',
+  templateUrl: './sc-alert.component.html',
+  styleUrls: ['./sc-alert.component.css']
 })
-export class SigninComponent implements OnInit   {
-
-
-  constructor(private authService: AuthService) { }
+export class ScAlertComponent implements OnInit {
     private _success = new Subject<string>();
-
     staticAlertClosed = false;
     successMessage: string;
+
+  constructor() { }
+
     ngOnInit(): void {
         setTimeout(() => this.staticAlertClosed = true, 20000);
 
         this._success.subscribe((message) => this.successMessage = message);
         this._success.debounceTime(5000).subscribe(() => this.successMessage = null);
     }
+
     public changeSuccessMessage() {
         this._success.next(`${new Date()} - Message successfully changed.`);
     }
 
-  onSignin(form: NgForm) {
-    const email = form.value.email;
-    const password = form.value.password;
-    this.authService.signinUser(email, password);
-      this.changeSuccessMessage()
-  }
 
 }
